d82bda31d9b00ab98fdbe528663c89634abe3c9f
Long Nguyen <lpn@mit.edu>
2019-10-14 02:08:20 -0400

    [ps3] Runge-Kutta 4 integrator for better stability

--- assn3/starter3/src/timestepper.cpp
+++ assn3/starter3/src/timestepper.cpp
@@ -35,5 +35,42 @@ void Trapezoidal::takeStep(ParticleSystem *particleSystem, float stepSize) {
 
 
 void RK4::takeStep(ParticleSystem *particleSystem, float stepSize) {
+    float h = stepSize;
+    vector<Vector3f> k1, k2, k3, k4;
+    vector<Vector3f> x = particleSystem->getState();
+
+    k1 = particleSystem->evalF(x);
+    vector<Vector3f> hDiv2k1;
+    for (unsigned int i = 0; i < x.size(); ++i) {
+        hDiv2k1.push_back(
+            x[i] + (h / 2 * k1[i])
+        );
+    }
+
+    k2 = particleSystem->evalF(hDiv2k1);
+    vector<Vector3f> hDiv2k2;
+    for (unsigned int i = 0; i < x.size(); ++i) {
+        hDiv2k2.push_back(
+            x[i] + (h / 2 * k2[i])
+        );
+    }
+
+    k3 = particleSystem->evalF(hDiv2k2);
+    vector<Vector3f> hk3;
+    for (unsigned int i = 0; i < x.size(); ++i) {
+        hk3.push_back(
+            x[i] + (h * k3[i])
+        );
+    }
+
+    k4 = particleSystem->evalF(hk3);
+
+    vector<Vector3f> XPrime;
+    for (int i = 0; i < (int)x.size(); ++i) {
+        XPrime.push_back(
+            x[i] + (h / 6) * (k1[i] + 2 * (k2[i] + k3[i]) + k4[i])
+        );
+    }
+    particleSystem->setState(XPrime);
 }
 
