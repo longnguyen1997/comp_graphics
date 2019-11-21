dd521ee9993121a7764cca6c73f7ebace1817cb0
Long Nguyen <long@dhcp-18-21-163-78.dyn.mit.edu>
2019-11-21 15:04:39 -0500

    [ps5] Removed shadow comment

--- assn5/starter5/shaders/fragmentshader_dirlight.glsl
+++ assn5/starter5/shaders/fragmentshader_dirlight.glsl
@@ -68,7 +68,6 @@ void main () {
 
     vec4 color = vec4(ambientColor + blinn_phong(kd).xyz, 1);
     if (occluder_depth + 0.01 < this_depth) {
-        // Fragment in shadow. 60%.
         out_Color = this_depth * vec4(blinn_phong(kd).xyz, 1);
     } else {
         // Fragment illuminated.
